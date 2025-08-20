c2d893b01fc256a678bfadb158c95a74b7c951ca
Nikola Lekoski <nikolalekoski@gmail.com>
2025-08-20 17:03:14 +0200

    adding new rows WIP

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,6 +1,7 @@
 import { TextField, Box, MenuItem } from "@mui/material";
 import React from "react";
 import { EmovieSearchParam } from "../types/movie";
+import { Colors } from "../shared/colors.ts";
 
 interface SearchBarProps {
   searchTerm: string;
@@ -9,25 +10,32 @@ interface SearchBarProps {
   onSearchParamChange: (param: EmovieSearchParam) => void;
 }
 
-const SearchBar: React.FC<SearchBarProps> = ({ searchTerm, onSearchChange, searchParam, onSearchParamChange }) => {
+const SearchBar: React.FC<SearchBarProps> = ({
+  searchTerm,
+  onSearchChange,
+  searchParam,
+  onSearchParamChange,
+}) => {
   return (
     <Box
       sx={{
         display: "flex",
         mt: 5,
-      }}>
+      }}
+    >
       <Box
         sx={{
           display: "flex",
           borderRadius: 8,
-          boxShadow: "0 4px 12px rgba(0,0,0,0.1)",
+          boxShadow: `0 4px 12px ${Colors.primary.black}`,
           alignItems: "center",
           px: 4,
           transition: "box-shadow 0.3s ease",
           "&:hover": {
-            boxShadow: "0 6px 16px rgba(0,0,0,0.15)",
+            boxShadow: `0 6px 16px ${Colors.secondary.black}`,
           },
-        }}>
+        }}
+      >
         <TextField
           variant="standard"
           placeholder="Search movies"
@@ -46,7 +54,9 @@ const SearchBar: React.FC<SearchBarProps> = ({ searchTerm, onSearchChange, searc
           select
           variant="standard"
           value={searchParam}
-          onChange={(e) => onSearchParamChange(e.target.value as EmovieSearchParam)}
+          onChange={(e) =>
+            onSearchParamChange(e.target.value as EmovieSearchParam)
+          }
           InputProps={{
             disableUnderline: true,
             sx: {
@@ -64,7 +74,8 @@ const SearchBar: React.FC<SearchBarProps> = ({ searchTerm, onSearchChange, searc
             "& .MuiMenuItem-root": {
               fontSize: "1rem",
             },
-          }}>
+          }}
+        >
           {Object.values(EmovieSearchParam).map((value, index) => (
             <MenuItem key={index} value={value}>
               {value}
--- src/shared/colors.ts
+++ src/shared/colors.ts
@@ -1,4 +1,4 @@
 export const Colors = {
-  primary: { white: "f8f9fa" },
-  secondary: {},
+  primary: { white: "f8f9fa", black: "rgba(0,0,0,0.1)" },
+  secondary: { black: "rgba(0,0,0,0.15)" },
 };
